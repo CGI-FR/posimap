24dfdcf697e12373e9ead1f860aefc2e5dfc776c
Adrien Aury <adrien.aury@cgi.com>
2025-04-30 15:34:38 +0000

    chore: add benchmark tests

---
+++ internal/appli/command/fold_test.go
@@ -0,0 +1,31 @@
+package command_test
+
+import (
+	"bytes"
+	"os"
+	"testing"
+
+	"github.com/cgi-fr/posimap/internal/appli/command"
+	"github.com/rs/zerolog"
+)
+
+func BenchmarkFold(b *testing.B) {
+	zerolog.SetGlobalLevel(zerolog.WarnLevel)
+
+	// File is loaded once before the loop to avoid benchmarking I/O operations
+	datafile, err := os.ReadFile("testdata/data.fixed-width")
+	if err != nil {
+		b.Fatalf("Failed to open test data file: %v", err)
+	}
+
+	for b.Loop() {
+		command := command.NewFoldCommand("posimap", "testgroup")
+		command.SetArgs([]string{"-c", "testdata/schema.yaml"})
+		command.SetIn(bytes.NewReader(datafile))
+		command.SetOut(os.Stdout)
+
+		if err := command.Execute(); err != nil {
+			b.Fatalf("Failed to execute command: %v", err)
+		}
+	}
+}
