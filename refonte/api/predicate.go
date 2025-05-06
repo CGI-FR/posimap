9b09a97652723b669791e288a56ff328997fff29
Adrien Aury <adrien.aury@cgi.com>
2025-05-06 16:54:03 +0000

    refactor: wip! predicate

--- refonte/api/predicate.go
+++ refonte/api/predicate.go
@@ -1,3 +1,40 @@
 package api
 
-type Predicate func(root any) bool
+import (
+	"bytes"
+	"html/template"
+
+	"github.com/Masterminds/sprig/v3"
+)
+
+type Predicate func(root Record) bool
+
+func If(value bool) Predicate {
+	return func(_ Record) bool { return value }
+}
+
+func Always() Predicate {
+	return func(_ Record) bool { return true }
+}
+
+func Never() Predicate {
+	return func(_ Record) bool { return false }
+}
+
+func When(tmpl string) Predicate {
+	if tmpl == "" {
+		return nil
+	}
+
+	template, err := template.New("predicate").Funcs(sprig.TxtFuncMap()).Parse(tmpl)
+	if err != nil {
+		panic(err)
+	}
+
+	return func(root Record) bool {
+		var result bytes.Buffer
+		_ = template.Execute(&result, root.AsPrimitive())
+
+		return result.String() != "false"
+	}
+}
