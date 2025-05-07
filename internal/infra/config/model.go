09b561b679d3b812f162714cbafe73a01fe97ea0
Adrien Aury <adrien.aury@cgi.com>
2025-05-07 16:33:32 +0000

    refactor: wip! config

--- internal/infra/config/model.go
+++ internal/infra/config/model.go
@@ -1,133 +1,20 @@
-// Copyright (C) 2025 CGI France
-//
-// This file is part of posimap.
-//
-// posimap is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// posimap is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with posimap.  If not, see <http://www.gnu.org/licenses/>.
-
 package config
-
-import (
-	"errors"
-	"fmt"
-
-	"github.com/cgi-fr/posimap/pkg/data"
-	"gopkg.in/yaml.v3"
-)
 
 type Config struct {
 	Schema Schema `yaml:"schema"`
-	Trim   bool   `yaml:"trim,omitempty"`
 }
 
 type Schema []Field
 
-type Either[T1 any, T2 any] struct {
-	T1 T1
-	T2 T2
-}
-
-func (e *Either[T1, T2]) UnmarshalYAML(value *yaml.Node) error {
-	out, err := yaml.Marshal(value)
-	if err != nil {
-		return fmt.Errorf("%w", err)
-	}
-
-	if err := yaml.Unmarshal(out, &e.T1); err == nil {
-		return nil
-	}
-
-	if err := yaml.Unmarshal(out, &e.T2); err == nil {
-		return nil
-	}
-
-	return fmt.Errorf("%w", err)
-}
-
 type Field struct {
-	Name     string                 `yaml:"name"`
-	Length   int                    `yaml:"length"`
-	Occurs   int                    `yaml:"occurs,omitempty"`
-	Redefine string                 `yaml:"redefine,omitempty"`
-	Trim     bool                   `yaml:"trim,omitempty"`
-	When     string                 `yaml:"when,omitempty"`
-	Schema   Either[string, Schema] `yaml:"schema,omitempty"` // either filename for external schema or inlined schema
-	schema   Schema
-}
+	Name     string `yaml:"name"`
+	Occurs   int    `yaml:"occurs,omitempty"`
+	Redefine string `yaml:"redefine,omitempty"`
+	When     string `yaml:"when,omitempty"`
 
-var (
-	ErrFieldNameEmpty     = errors.New("field name cannot be empty")
-	ErrFieldLengthZero    = errors.New("field length cannot be zero")
-	ErrFieldStartNegative = errors.New("field start position cannot be negative")
-)
+	Length  int    `yaml:"length"`
+	Trim    bool   `yaml:"trim,omitempty"`
+	Charset string `yaml:"charset,omitempty"`
 
-func (f Field) Validate() error {
-	if f.Name == "" {
-		return ErrFieldNameEmpty
-	}
-
-	return nil
-}
-
-func (f Field) Build(trim bool) data.FieldSchema {
-	return data.FieldSchema{
-		Name:     f.Name,
-		Length:   f.Length,
-		Occurs:   f.Occurs,
-		Redefine: f.Redefine,
-		Trim:     f.Trim || trim,
-		When:     data.When(f.When),
-		Schema:   f.schema.Compile(trim),
-	}
-}
-
-func (s Schema) Validate() error {
-	for idx, field := range s {
-		if err := field.Validate(); err != nil {
-			return fmt.Errorf("%v.%w", idx, err)
-		}
-
-		if field.schema != nil {
-			if err := field.schema.Validate(); err != nil {
-				return fmt.Errorf("%v.%w", idx, err)
-			}
-		}
-	}
-
-	return nil
-}
-
-func (s Schema) Compile(trim bool) data.RecordSchema {
-	if len(s) == 0 {
-		return nil
-	}
-
-	result := make(data.RecordSchema, len(s))
-	for i, field := range s {
-		result[i] = field.Build(trim)
-	}
-
-	return result
-}
-
-func (c Config) Validate() error {
-	if err := c.Schema.Validate(); err != nil {
-		return fmt.Errorf("schema validation failed: %w", err)
-	}
-
-	return nil
-}
-
-func (c Config) Compile() data.RecordSchema {
-	return c.Schema.Compile(c.Trim)
+	Schema Either[string, Schema] `yaml:"schema"` // either filename for external schema or embedded schema
 }
