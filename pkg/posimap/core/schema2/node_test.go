a6a262a66a7ce2e075bba5ece161085c84661868
Adrien Aury <44274230+adrienaury@users.noreply.github.com>
2025-05-11 09:47:50 +0000

    refactor: wip! test expected output

--- pkg/posimap/core/schema2/node_test.go
+++ pkg/posimap/core/schema2/node_test.go
@@ -39,4 +39,51 @@ func Example() {
 	root.PrintGraph()
 
 	// Output:
+	// digraph ROOT {
+	// 	node [shape = box fixedsize=true width=3];
+	// 	"ROOT" [label = "ROOT\n69"];
+	// 	"ID" [label = "ID\n16"];
+	// 	"ROOT" -> "ID";
+	// 	"ROOT" [label = "ROOT\n69"];
+	// 	"PERSON" [label = "PERSON\n16"];
+	// 	"ROOT" -> "PERSON";
+	// 	"PERSON" [label = "PERSON\n16"];
+	// 	"FIRSTNAME" [label = "FIRSTNAME\n8"];
+	// 	"PERSON" -> "FIRSTNAME";
+	// 	"PERSON" [label = "PERSON\n16"];
+	// 	"LASTNAME" [label = "LASTNAME\n8"];
+	// 	"PERSON" -> "LASTNAME";
+	// 	"LASTNAME" -> "FIRSTNAME" [style=dashed constraint=false color=red label=8];
+	// 	"PERSON" -> "LASTNAME" [style=dashed constraint=false color=red label=16];
+	// 	"ROOT" [label = "ROOT\n69"];
+	// 	"COMPANY" [label = "COMPANY\n16"];
+	// 	"ROOT" -> "COMPANY";
+	// 	"COMPANY" [label = "COMPANY\n16"];
+	// 	"NAME" [label = "NAME\n16"];
+	// 	"COMPANY" -> "NAME";
+	// 	"COMPANY" -> "NAME" [style=dashed constraint=false color=red label=16];
+	// 	"ROOT" [label = "ROOT\n69"];
+	// 	"ADDRESSES" [label = "ADDRESSES\n50"];
+	// 	"ROOT" -> "ADDRESSES";
+	// 	"ADDRESSES" [label = "ADDRESSES\n50"];
+	// 	"LINE-1" [label = "LINE-1\n25"];
+	// 	"ADDRESSES" -> "LINE-1";
+	// 	"LINE-1" -> "ID" [style=dashed constraint=false color=red label=16];
+	// 	"LINE-1" -> "PERSON" [style=dashed constraint=false color=red label=16];
+	// 	"LINE-1" -> "COMPANY" [style=dashed constraint=false color=red label=16];
+	// 	"ADDRESSES" [label = "ADDRESSES\n50"];
+	// 	"LINE-2" [label = "LINE-2\n25"];
+	// 	"ADDRESSES" -> "LINE-2";
+	// 	"LINE-2" -> "LINE-1" [style=dashed constraint=false color=red label=41];
+	// 	"ADDRESSES" -> "LINE-2" [style=dashed constraint=false color=red label=66];
+	// 	"ROOT" [label = "ROOT\n69"];
+	// 	"ISCOMPANY" [label = "ISCOMPANY\n1"];
+	// 	"ROOT" -> "ISCOMPANY";
+	// 	"ISCOMPANY" -> "ADDRESSES" [style=dashed constraint=false color=red label=66];
+	// 	"ROOT" [label = "ROOT\n69"];
+	// 	"TITLES" [label = "TITLES\n2"];
+	// 	"ROOT" -> "TITLES";
+	// 	"TITLES" -> "ISCOMPANY" [style=dashed constraint=false color=red label=67];
+	// 	"ROOT" -> "TITLES" [style=dashed constraint=false color=red label=69];
+	// }
 }
