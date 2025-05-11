f8632afee59a593e171d6d0d25e05b7dee527e42
Adrien Aury <44274230+adrienaury@users.noreply.github.com>
2025-05-11 09:02:59 +0000

    refactor: wip! schema graph modelisation

---
+++ pkg/posimap/core/schema2/node.go
@@ -0,0 +1,108 @@
+package schema2
+
+import (
+	"fmt"
+
+	"github.com/cgi-fr/posimap/pkg/posimap/api"
+)
+
+type node struct {
+	name      string
+	redefines string
+	occurs    int
+	when      api.Predicate
+
+	redefined map[string]*node
+	children  []*node
+	dependsOn []*node
+}
+
+func (n *node) setDependsOn(other *node) {
+	backup := n.dependsOn
+	n.dependsOn = []*node{other}
+	other.dependsOn = backup
+}
+
+func (n *node) addDependsOn(other *node) {
+	n.dependsOn = append(n.dependsOn, other)
+	dependent := n.redefined[other.name]
+	other.dependsOn = dependent.dependsOn
+}
+
+func (n *node) addChild(other *node) {
+	n.children = append(n.children, other)
+
+	if other.redefines == "" {
+		n.redefined[other.name] = other
+	} else {
+		n.redefined[other.name] = n.redefined[other.redefines]
+	}
+}
+
+func (n *node) PrintGraph() {
+	for _, child := range n.children {
+		fmt.Printf("\t\"%s\" -> \"%s\";\n", n.name, child.name)
+		child.PrintGraph()
+	}
+
+	for _, dep := range n.dependsOn {
+		fmt.Printf("\t\"%s\" -> \"%s\" [style=dashed constraint=false color=red];\n", n.name, dep.name)
+	}
+}
+
+type Field struct {
+	*node
+
+	codec api.Codec[any]
+}
+
+func NewField(name string, codec api.Codec[any]) *Field {
+	return &Field{
+		node: &node{
+			name:      name,
+			redefines: "",
+			occurs:    0,
+			when:      nil,
+			redefined: make(map[string]*node),
+			children:  []*node{},
+			dependsOn: []*node{},
+		},
+		codec: codec,
+	}
+}
+
+type Record struct {
+	*node
+}
+
+func NewRecord(name string) *Record {
+	return &Record{
+		node: &node{
+			name:      name,
+			redefines: "",
+			occurs:    0,
+			when:      nil,
+			redefined: make(map[string]*node),
+			children:  []*node{},
+			dependsOn: []*node{},
+		},
+	}
+}
+
+func (r *Record) AddField(field *Field) {
+	r.addChild(field.node)
+}
+
+func (r *Record) AddRecord(record *Record) {
+	r.addChild(record.node)
+}
+
+func (r *Record) PrintGraph() {
+	fmt.Printf("digraph %s {\n", r.name)
+
+	fmt.Printf("\tnode [shape = box fixedsize=true width=3];\n")
+
+	r.node.PrintGraph()
+
+	fmt.Printf("}\n")
+}
---
+++ pkg/posimap/core/schema2/node_test.go
@@ -0,0 +1,42 @@
+package schema2_test
+
+import (
+	"github.com/cgi-fr/posimap/pkg/posimap/core/codec"
+	"github.com/cgi-fr/posimap/pkg/posimap/core/schema2"
+	"golang.org/x/text/encoding/charmap"
+)
+
+func Example() {
+	root := schema2.NewRecord("ROOT")
+
+	rootID := schema2.NewField("ID", codec.NewString(charmap.ISO8859_1, 16, false))
+	personFirstname := schema2.NewField("FIRSTNAME", codec.NewString(charmap.ISO8859_1, 8, true))
+	personLastname := schema2.NewField("LASTNAME", codec.NewString(charmap.ISO8859_1, 8, true))
+	companyName := schema2.NewField("NAME", codec.NewString(charmap.ISO8859_1, 16, true))
+	addressesLine1 := schema2.NewField("LINE-1", codec.NewString(charmap.ISO8859_1, 25, true))
+	addressesLine2 := schema2.NewField("LINE-2", codec.NewString(charmap.ISO8859_1, 25, true))
+	rootIscompany := schema2.NewField("ISCOMPANY", codec.NewString(charmap.ISO8859_1, 1, true))
+	rootTitles := schema2.NewField("TITLES", codec.NewString(charmap.ISO8859_1, 2, true))
+
+	person := schema2.NewRecord("PERSON")
+	person.AddField(personFirstname)
+	person.AddField(personLastname)
+
+	company := schema2.NewRecord("COMPANY")
+	company.AddField(companyName)
+
+	addresses := schema2.NewRecord("ADDRESSES")
+	addresses.AddField(addressesLine1)
+	addresses.AddField(addressesLine2)
+
+	root.AddField(rootID)
+	root.AddRecord(person)
+	root.AddRecord(company)
+	root.AddRecord(addresses)
+	root.AddField(rootIscompany)
+	root.AddField(rootTitles)
+
+	root.PrintGraph()
+
+	// Output:
+}
