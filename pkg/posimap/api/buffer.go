21b020b51a2918d2a63554e384e14d4c494fa1cc
Adrien Aury <adrien.aury@cgi.com>
2025-05-14 12:13:43 +0000

    feat: records separator

--- pkg/posimap/api/buffer.go
+++ pkg/posimap/api/buffer.go
@@ -4,6 +4,5 @@ type Buffer interface {
 	Slice(offset, length int) ([]byte, error)
 	Write(offset int, data []byte) error
 	Reset(size int, separator ...byte) error
-	LockTo(separator []byte) error
 	Fill(byteValue byte)
 }
