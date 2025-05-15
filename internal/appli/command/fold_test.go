059e29941033f68a04a38cdc434dc428eddbce2d
Adrien Aury <adrien.aury@cgi.com>
2025-05-15 08:40:18 +0000

    test: feedback parameter

--- internal/appli/command/fold_test.go
+++ internal/appli/command/fold_test.go
@@ -53,4 +53,5 @@ func TestFold(t *testing.T) {
 	t.Run("09-trim", func(t *testing.T) { RunFoldTestFromFile(t, "09-trim.yaml") })
 	t.Run("10-charsets", func(t *testing.T) { RunFoldTestFromFile(t, "10-charsets.yaml") })
 	t.Run("99-eof-short", func(t *testing.T) { RunFoldTestFromFile(t, "99-eof-short.yaml") })
+	t.Run("99-feedback", func(t *testing.T) { RunFoldTestFromFile(t, "99-feedback.yaml") })
 }
